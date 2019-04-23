b9d0f5a3b859170e3e9394e01428be09fafcafd3
DavidZh0u <admin0104130914>
2019-04-24 00:20:23 +0800

    结构改变

---
+++ MybatisGeneratorUtil/src/main/java/com/paladin/MybatisGeneratorUtilApplication.java
@@ -0,0 +1,13 @@
+package com.paladin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGeneratorUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGeneratorUtilApplication.class, args);
+    }
+
+}
